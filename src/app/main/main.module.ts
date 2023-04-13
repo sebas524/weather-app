faa5fecc649a731f4fa9797f33952bb1507080c6
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-13 17:09:37 +0200

    implement getWeatherByCity

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,9 +7,15 @@ import { SharedModule } from '../shared/shared.module';
 import { WeatherInfoMainPageComponent } from './main-layout/pages/weather-info-main-page/weather-info-main-page.component';
 import { SidebarWeatherComponent } from './components/sidebar-weather/sidebar-weather.component';
 import { MainContentWeatherComponent } from './components/main-content-weather/main-content-weather.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [MainLayoutComponent, WeatherInfoMainPageComponent, SidebarWeatherComponent, MainContentWeatherComponent],
-  imports: [CommonModule, MainRoutingModule, SharedModule],
+  declarations: [
+    MainLayoutComponent,
+    WeatherInfoMainPageComponent,
+    SidebarWeatherComponent,
+    MainContentWeatherComponent,
+  ],
+  imports: [CommonModule, MainRoutingModule, SharedModule, HttpClientModule],
 })
 export class MainModule {}
--- src/app/main/services/my-weather.service.ts
+++ src/app/main/services/my-weather.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
+import { WeatherApiInterface } from '../interfaces/weatherAPI.interface';
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyWeatherService {
+  private baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-  constructor() { }
+  constructor(public http: HttpClient) {}
+
+  getWeatherByCity(city: string): Observable<WeatherApiInterface | undefined> {
+    return this.http
+      .get<WeatherApiInterface>(
+        `${this.baseUrl}?q=${city}&appid=${environment.APIKEY}`
+      )
+      .pipe(
+        catchError((err) => {
+          return of(undefined);
+        })
+      );
+  }
 }
