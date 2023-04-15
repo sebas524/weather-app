5c1e2cc721f9afb14501943bbd5ee6ce0a703c30
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-15 10:20:58 +0200

    implement geoLocation and new api request

--- src/app/main/pages/weather-info-main-page/weather-info-main-page.component.ts
+++ src/app/main/pages/weather-info-main-page/weather-info-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MyWeatherService } from '../../services/my-weather.service';
 import { WeatherApiInterface } from 'src/app/main/interfaces/weatherAPI.interface';
 
@@ -7,20 +7,78 @@ import { WeatherApiInterface } from 'src/app/main/interfaces/weatherAPI.interfac
   templateUrl: './weather-info-main-page.component.html',
   styles: [],
 })
-export class WeatherInfoMainPageComponent {
-  public fetchedWeatherInfo?: WeatherApiInterface;
-  public imgNum: string = '10';
-  public iconUrl: string = `https://openweathermap.org/img/wn/${this.imgNum}d@2x.png`;
+export class WeatherInfoMainPageComponent implements OnInit {
+  // public fetchedWeatherInfo?: WeatherApiInterface;
+  // public imgNum: string = '10';
+  // public iconUrl: string = `https://openweathermap.org/img/wn/${this.imgNum}d@2x.png`;
 
-  // * injecting MyWeatherService:
-  constructor(private MyWeatherService: MyWeatherService) {}
+  // ! ATTRIBUTES:
+  // * timeline array will hold the time and corresponding temperature for the whole day (that means, 8 different forecasts, because it is every three hours. 8*3 = 24):
+  timeline: any = [];
+  weatherNow: any;
+  // * current time will return time and date of laptop:
+  currentTime = new Date();
+  // *holds city and location
+  location: any;
 
-  searchCityWeather(city: string) {
-    console.log('capital being searched: ', city);
+  // ! CONSTRUCTOR:
 
-    this.MyWeatherService.getWeatherByCity(city).subscribe((data) => {
-      console.log('fetched info =>', data);
-      this.fetchedWeatherInfo = data;
+  constructor(private forecastService: MyWeatherService) {}
+  // ! ON INIT:
+
+  ngOnInit(): void {
+    this.forecastService.getWeatherForecast().subscribe((data) => {
+      console.log('data from ngOnInit => ', data);
+      this.getTodayForecast(data);
     });
   }
+  // ! METHODS:
+
+  // * to compare:
+  dateRange() {
+    // * it will always return a range between 2 dates, that have a difference of 3 hours -1sec: 2:59
+    const start = new Date();
+    // ? error that i did not get:
+    start.setHours(start.getHours() + start.getTimezoneOffset() / 60);
+    // ? --------
+
+    const to = new Date(start);
+    to.setHours(to.getHours() + 2, to.getMinutes() + 59, to.getSeconds() + 59);
+    return { start, to };
+  }
+
+  // *the following method is to get the data stored in ngOnInit, it is then called inside the init with data passed in as parameter:
+  getTodayForecast(today: any) {
+    this.location = today.city;
+    // * we'll look through list and slice the data so we can get just the first 8 elements:
+    for (const forecast of today.list.slice(0, 8)) {
+      console.log(forecast);
+      this.timeline.push({
+        time: forecast.dt_txt,
+        temp: forecast.main.temp,
+      });
+
+      // * this const will be assign to it the dates provided to us by api, and then we'll change it to a timestamp:
+      const apiDate = new Date(forecast.dt_txt).getTime();
+
+      // * if our time(within our current computer time) is within 3 hours of the time provided by us by the api
+      if (
+        this.dateRange().start.getTime() <= apiDate &&
+        this.dateRange().to.getTime() >= apiDate
+      ) {
+        // * if these two conditions satisfy, then we will assign data to property called weatherNow(line 13):
+        this.weatherNow = forecast;
+        console.log('weather now => ', this.weatherNow);
+      }
+    }
+  }
+
+  // searchCityWeather(city: string) {
+  //   console.log('capital being searched: ', city);
+
+  //   this.MyWeatherService.getWeatherByCity(city).subscribe((data) => {
+  //     console.log('fetched info =>', data);
+  //     this.fetchedWeatherInfo = data;
+  //   });
+  // }
 }
--- src/app/main/services/my-weather.service.ts
+++ src/app/main/services/my-weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
 import { WeatherApiInterface } from '../interfaces/weatherAPI.interface';
 import { environment } from 'src/app/environments/environment';
 
@@ -12,6 +12,40 @@ export class MyWeatherService {
 
   constructor(public http: HttpClient) {}
 
+  // * method to generate weather
+  getWeatherForecast() {
+    return new Observable((observer) => {
+      // * The Geolocation.getCurrentPosition() method is used to get the current position of the device.
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // * position here will return the coordinates.
+          observer.next(position);
+        },
+        (err) => {
+          observer.next(err);
+        }
+      );
+    }).pipe(
+      // * in map we transform data. we need to append query parameters.
+      map((val: any) => {
+        // * remember you have already seen httpParams with fer!
+        return new HttpParams()
+          .set('lon', val.coords.longitude)
+          .set('lat', val.coords.latitude)
+          .set('units', 'metric')
+          .set('appid', 'bb3a234039078f99acd82d0fce2f1c34');
+      }),
+      // * switchMap will be used to fetch the data:
+      switchMap((values) => {
+        return this.http.get(
+          'https://api.openweathermap.org/data/2.5/forecast',
+          { params: values }
+        );
+      })
+    );
+  }
+
+  // !METHOD X
   getWeatherByCity(city: string): Observable<WeatherApiInterface | undefined> {
     const type: string = 'weather';
     return this.http
