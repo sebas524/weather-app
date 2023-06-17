105d5e63086d32de4a1d91479c85a345e2fe611d
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-06-17 18:04:37 +0200

    add map part 4

--- src/app/main/components/map/map.component.ts
+++ src/app/main/components/map/map.component.ts
@@ -1,6 +1,14 @@
-import { AfterViewInit, Component, Input, SimpleChanges } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
+import { Map } from 'mapbox-gl';
+
 import { environment } from 'src/app/environments/environment';
 
 (mapboxgl as any).accessToken = environment.MAPBOX_KEY;
@@ -10,16 +18,24 @@ import { environment } from 'src/app/environments/environment';
   templateUrl: './map.component.html',
   styles: [],
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements AfterViewInit, AfterViewChecked {
   @Input() lng: number = 0;
   @Input() lat: number = 0;
 
+  public map?: Map;
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map', // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [this.lng, this.lat], // starting position [lng, lat]
       zoom: 7, // starting zoom
     });
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.map) throw 'El elemento HTML no fue encontrado';
+
+    this.map.setCenter([this.lng, this.lat]);
   }
 }
